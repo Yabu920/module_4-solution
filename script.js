2429f10c6410c403ad5031c25f224a50e3e9633b
Yabu920 <159709633+Yabu920@users.noreply.github.com>
2024-03-12 23:07:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var names = ["yeabsra", "Abel", "jemal", "hayilu" , "jemila"];
+
+names.forEach(names => {
+  if(names.charAt(0) === 'j'){
+    console.log("GoodBye " + names);
+  }
+  else{
+    console.log("Hi " + names);
+  }
+})
